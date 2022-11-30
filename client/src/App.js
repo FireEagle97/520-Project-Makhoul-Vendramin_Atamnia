9f538ece15eb56febc8f7a79f6a968eacf53d102
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-11-30 15:38:52 -0500

    marker now showing on map

--- client/src/App.js
+++ client/src/App.js
@@ -31,10 +31,17 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: "500px", width: "100%"}}>
+    // center={position} zoom={13} scrollWheelZoom={false} style={{height: "500px", width: "100%"}}
+    <MapContainer 
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100vh" }}
+        >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      
     />
     <Marker position={position}>
       <Popup>
