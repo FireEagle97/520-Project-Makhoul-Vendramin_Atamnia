b323601e0640e7624e87b7a7bd553c3ac66b2c28
AdamAtamnia <adam18102002.aa@gmail.com>
2022-12-10 20:59:58 -0500

    test, fix, and modify seed.js (works now)

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 "use strict"
-require("dotenv").config();
-const dbUrl = process.env.ATLAS_URI;
-const { MongoClient } = require("mongodb");
+require("dotenv").config()
+const dbUrl = process.env.ATLAS_URI
+const { MongoClient } = require("mongodb")
 
 let instance
 
@@ -14,41 +14,51 @@ class DB{
   constructor(){
     //instance is the singleton, defined in outer scope
     if (!instance){
-      instance = this;
-      this.client = new MongoClient(dbUrl);
-      this.db = null;
-      this.collection = null;
+      instance = this
+      this.client = new MongoClient(dbUrl)
+      this.db = null
+      this.collection = null
     }
     return instance;
   }
 
   async connect(dbname, collName) {
     if (instance.db){
-      return;
+      return
     }
-    await instance.client.connect();
-    instance.db = await instance.client.db(dbname);
-    console.log("Successfully connected to MongoDB database " + dbname);
+    await instance.client.connect()
+    instance.db = await instance.client.db(dbname)
+    console.log("Successfully connected to MongoDB database " + dbname)
     instance.collection = await instance.db.collection(collName)
   }
 
   async close() {
-    await instance.client.close();
-    instance = null;
+    await instance.client.close()
+    instance = null
+  }
+
+  async find(query) {
+    // projection if 0 means dont return this field, 1 means return it
+    return await instance.collection.find(query).projection({ _id: 0 }).toArray()
+    
   }
 
   async readAll() {
     // projection if 0 means dont return this field, 1 means return it
-    return await instance.collection.find().projection({ _id: 0 }).toArray();
+    return await instance.collection.find().projection({ _id: 0 }).toArray()
     
   }
 
   async insert(busRoute) {
-    return await instance.collection.insertOne(busRoute);
+    return await instance.collection.insertOne(busRoute)
   }
 
   async insertMany(busRoutes) {
-    return await instance.collection.insertMany(busRoutes);
+    return await instance.collection.insertMany(busRoutes)
+  }
+
+  async deleteAll() {
+    return await instance.collection.deleteMany({})
   }
 }
 
--- server/database/seed.js
+++ server/database/seed.js
@@ -1,5 +1,25 @@
 "use strict"
 
+/**
+ * {
+    routeId: '138',
+    routeLongName: 'Notre-Dame-de-Grâce',
+    routeColor: '009EE0',
+    direction1: {
+      headsign: '138-O',
+      tripIds: [Array],
+      shapes: [Array],
+      stops: [Array]
+    },
+    direction0: {
+      headsign: '138-E',
+      tripIds: [Array],
+      shapes: [Array],
+      stops: [Array]
+    }
+  },
+ */
+
 const DB = require("./db")
 
 const fs = require("fs");
@@ -25,21 +45,36 @@ const gtfsFolderPath = "../../gtfs_stm/"
 const seenRoutesSet = new Set()
 const shapeIdToRouteIdCommaDirection = new Map()
 const tripIdToRouteIdCommaDirection = new Map()
-const stopIdToRouteIdCommaDirection = new Map()
+const stopIdToRouteIdCommaDirections = new Map()
 
 
-parseRoutes(
-  parseTrips(
-    parseShapes(
-      parseStopTimes(
+parseRoutes(() =>
+  parseTrips(() =>
+    parseShapes(() =>
+      parseStopTimes(() =>
         parseStops(() => {
           console.log("finished parsing")
+          console.log(busRoutes.values())
+          // console.log(numTotalShapes)
+          // console.log(numTotalDifferingShapes)
+          // console.dir(Array.from(busRoutes.values())[100].direction1.tripIds)
+          // console.dir(Array.from(busRoutes.values())[100].direction1.shapes)
+          // console.dir(Array.from(busRoutes.values())[100].direction1.stops)
+
+          // console.dir(Array.from(busRoutes.values())[100].direction0.tripIds)
+          // console.dir(Array.from(busRoutes.values())[100].direction0.shapes)
+          // console.dir(Array.from(busRoutes.values())[100].direction0.stops)
+
+          // console.log(numTotalStops)
+          // console.log(numTotalDifferingStops)
+
           addBusRoutesToDB()
         })
       )
     )
   )
 )
+
 
 
 function parseRoutes(finishedCallback){
@@ -49,11 +84,13 @@ function parseRoutes(finishedCallback){
       routeId: row[0],
       routeLongName: row[3],
       routeColor: row[6]
+
     })
   
     console.log(row);
   }, finishedCallback)
 }
+
 
 function parseTrips(finishedCallback){
   parseCSV(gtfsFolderPath + "trips.txt", row => {
@@ -63,17 +100,18 @@ function parseTrips(finishedCallback){
       seenRoutesSet.add(row[0] + row[4])
       busRoutes.get(row[0])["direction" + row[4]] = {
         headsign: row[3],
-        // maybe not needed
-        tripId: row[2],
-        shapeId: row[5], 
-        // ---------------
+        tripIds: [row[2]],
         shapes: [],
         stops: []
       }
+      // some trips with the same route and direction have different shapes, 
+      // like shape: 410317
+      // but we are only going to take the first one
       shapeIdToRouteIdCommaDirection.set(row[5], row[0] + "," + row[4])
       tripIdToRouteIdCommaDirection.set(row[2], row[0] + "," + row[4])
       console.log(row);
-
+    } else {
+      busRoutes.get(row[0])["direction" + row[4]].tripIds.push(row[2])
     }
     
   }, finishedCallback)
@@ -82,16 +120,20 @@ function parseTrips(finishedCallback){
 function parseShapes(finishedCallback) {
   parseCSV(gtfsFolderPath + "shapes.txt", row => {
     //0: shape_id, 1: lat, 2: lon
-  
-    const routeId = shapeIdToRouteIdCommaDirection.get(row[0]).split(",")[0]
-    const directionId = shapeIdToRouteIdCommaDirection.get(row[0]).split(",")[1]
-  
-    busRoutes.get(routeId)["direction" + directionId].shapes.push({
-      lat: row[1],
-      lon: row[2]
-    })
+
+    if(shapeIdToRouteIdCommaDirection.has(row[0])){
+      const routeId = shapeIdToRouteIdCommaDirection.get(row[0]).split(",")[0]
+      const directionId = shapeIdToRouteIdCommaDirection.get(row[0]).split(",")[1]
     
-    console.log(row);
+      busRoutes.get(routeId)["direction" + directionId].shapes.push({
+        lat: row[1],
+        lon: row[2]
+      })
+      
+      console.log(row);
+    } else {
+      console.log("Differing shape for same route and direction---->" + row)
+    }
   }, finishedCallback)
 }
 
@@ -100,11 +142,15 @@ function parseStopTimes(finishedCallback) {
   parseCSV(gtfsFolderPath + "stop_times.txt", row => {
     //0: trip_id, 3: stop_id
 
-    if(!tripIdToRouteIdCommaDirection.has(row[0])){
+    if(tripIdToRouteIdCommaDirection.has(row[0])){
       const routeId = tripIdToRouteIdCommaDirection.get(row[0]).split(",")[0]
       const directionId = tripIdToRouteIdCommaDirection.get(row[0]).split(",")[1]
-    
-      stopIdToRouteIdCommaDirection.set(row[3], routeId + "," + directionId)
+
+      if(!stopIdToRouteIdCommaDirections.has(row[3])){
+        stopIdToRouteIdCommaDirections.set(row[3], [routeId + "," + directionId])
+      } else {
+        stopIdToRouteIdCommaDirections.get(row[3]).push(routeId + "," + directionId)
+      }
       
       console.log(row);
     }
@@ -117,42 +163,38 @@ function parseStops(finishedCallback) {
   parseCSV(gtfsFolderPath + "stops.txt", row => {
     //0: stop_id, 2: stop_name, 3: lat, 4: lon  
 
-    const routeId = stopIdToRouteIdCommaDirection.get(row[0]).split(",")[0]
-    const directionId = stopIdToRouteIdCommaDirection.get(row[0]).split(",")[1]
-    
-    busRoutes.get(routeId)["direction" + directionId].stops.push({
-      name: row[2],
-      lat: row[3],
-      lon: row[4]
-    })
+    if(stopIdToRouteIdCommaDirections.has(row[0])){
 
-    console.log(row);
-    
+      stopIdToRouteIdCommaDirections.get(row[0]).forEach(string => {
+        const routeId = string.split(",")[0]
+        const directionId = string.split(",")[1]
+        
+        busRoutes.get(routeId)["direction" + directionId].stops.push({
+          name: row[2],
+          lat: row[3],
+          lon: row[4]
+        })
+      })
+
+      
+      console.log(row);
+
+    } else {
+      console.log("Differing stops---------------" + row);
+    }
   }, finishedCallback)
 }
 
-// what we want
-/**
- * route
- * other route info
- * 
- * south shape
- * south stops
- * 
- * north shape
- * north stops
- * 
- */
-
 async function addBusRoutesToDB() {
   try {
-    console.log("Adding bus routes to DB...");
-    const db = new DB();
-    await db.connect(dbname, collection);
-    let num = await db.createMany(busRoutes);
-    console.log(`Inserted ${num} bus routes`);
+    console.log("Adding bus routes to DB...")
+    const db = new DB()
+    await db.connect(dbname, collection)
+    await db.deleteAll()
+    await db.insertMany(Array.from(busRoutes.values()))
+    console.log(`Inserted bus routes`)
   } catch (e) {
-    console.error("could not connect");
+    console.error("could not insert the routes");
     process.exit();
   }
 }
