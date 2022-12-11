eef398521484cc30f850e48085708b2b87196c2c
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-12-11 18:06:50 -0500

    revert to default skin map

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
         >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     {busses.map((bus) =>
     <Marker position={[bus.position.latitude, bus.position.longitude]} eventHandlers={{
